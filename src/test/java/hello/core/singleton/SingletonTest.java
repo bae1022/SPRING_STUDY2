83426ff8be1d5a7a4ab1c892d4fc006ea8cd8b7b
bae1022 <shbk0423@naver.com>
2022-01-12 23:07:49 +0900

    update

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,50 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    /**
+     * 아래 코드는 비효율적
+     * -> 객체가 계속해서 새로 만들어지기 때문
+     */
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer(){
+        AppConfig appConfig = new AppConfig();
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService instance1 = SingletonService.getInstance();
+        SingletonService instance2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1=" + instance1);
+        System.out.println("singletonService2=" + instance2);
+
+        assertThat(instance1).isSameAs(instance2);
+        //same; ==
+        //equal; 자바의 equals
+
+    }
+}
