bd029e0f378cb026a33e25df6c6f8811b6bd915e
bae1022 <shbk0423@naver.com>
2022-01-13 16:56:50 +0900

    update

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -34,22 +34,29 @@ import org.springframework.context.annotation.Configuration;
 //}
 
 // Spring 으로 변환 (@configuration -> 애플리케이션의 설정 정보라는 의미)
-@Configuration
+@Configuration // 얘 없으면 memberRepository가 3번 호출되게 된다.
 public class AppConfig {
 
     // @bean 을 함으로써 스프링 컨테이너에 등록이 된다.
+
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
-    public MemoryMemberRepository memberRepository() {
+    public MemoryMemberRepository memberRepository() { // 한 번만 호출이 되었음
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
